43d8f8b0b6c20eaa2cf0c4181d766b70c5fdef60
sgon512 <126737582+sgon512@users.noreply.github.com>
2024-02-12 22:06:43 +1300

    Update server.js
    connections should be established before a server's method is called

--- server.js
+++ server.js
@@ -7,25 +7,32 @@ const bodyParser = require("body-parser");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
-connectDB();
 
 app.prepare().then(() => {
   const server = express();
 
+  // Define custom routes here
   server.get("/test", (req, res) => {
     res.send({ message: "This is a custom Express route!yyyyy" });
   });
+
   server.use(bodyParser.json());
   server.use("/api/v1/portfolios", require("./backend/routes/portfolios"));
+
+  // Handle all other routes with Next.js
   server.all("*", (req, res) => {
     return handle(req, res);
   });
 
   const PORT = process.env.PORT || 3000;
-  server.listen(PORT, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log(`> Ready on port ${PORT}`);
-  });
+
+  // Connect to DB and then start the server
+  (async function startServer() {
+    await connectDB();
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on port ${PORT}`);
+    });
+  })();
 });
+
